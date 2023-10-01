96d430a1bedab6d3f7e641a3d6c453a2fa1e3cd5
Erin.Schneider <erinsuzy@gmail.com>
2023-10-01 12:16:57 -0500

    views-static thymeleaf boilerplate

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -9,15 +9,15 @@ import org.springframework.web.bind.annotation.*;
 public class HelloController {
     //handles request at path /hello
     //@GetMapping("hello")
-    //@ResponseBody
-    //public String hello() {
-     //   return "Hello, Spring!";
-   // }
+    @ResponseBody
+    public String hello() {
+        return "Hello, Spring!";
+    }
 
     //lives /hello/goodbye
     @GetMapping("goodbye")
     public String goodbye() {
-        return "Goodbye, Spring!";
+       return "Goodbye, Spring!";
     }
 
     //Handles request of the form /hello/name=LaunchCode
@@ -28,21 +28,59 @@ public class HelloController {
 
     //Handles requests of the form /hello/LaunchCode
     @GetMapping("{name}")
-
     public String helloWithPathParam(@PathVariable String name){
-        return "Hello, " + name + "!";
+       return "Hello, " + name + "!";
     }
 
     //lives at /hello/form
     @GetMapping("form")
     public String helloForm() {
-        return "<html>" +
+        String html = "<html>" +
                 "<body>" +
                 "<form action= 'hello' method= 'post'>" + //submit a request to /hello
                 "<input type = 'text' name= name>" +
-                "<input type= 'submit' value= 'Greet me!'>" +
+                "<input type = 'submit' value = 'Greet Me!'>" +
                 "</form>" +
                 "</body>" +
                 "</html>";
+        return html;
     }
+
+    /*@RequestMapping(value="hello", method = RequestMethod.POST)
+    @ResponseBody
+    public String helloPost(@RequestParam String name, @RequestParam String language) {
+        if (name == null) {
+            name = "World";
+        }
+
+        return createMessage(name, language);
+
+        // For a bonus mission, students can change this response text to look nicer.
+        // This is subjective, but students should be modifying the HTML of the response string.
+    }
+
+    public static String createMessage(String n, String l) {
+        String greeting = "";
+
+        if (l.equals("english")) {
+            greeting = "Hello";
+        }
+        else if (l.equals("french")) {
+            greeting = "Bonjour";
+        }
+        else if (l.equals("italian")) {
+            greeting = "Bonjourno";
+        }
+        else if (l.equals("spanish")) {
+            greeting = "Hola";
+        }
+        else if (l.equals("german")) {
+            greeting = "Hallo";
+        }
+
+        return greeting + " " + n;
+    }
+
+     */
 }
+
